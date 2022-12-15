a555b944b16080f80285bbd7dc2620e9bd727c94
Vladislav Viktorov CSE 37 group <115734573+VladislavViktorov@users.noreply.github.com>
2022-12-15 23:53:02 +0200

    Update sem-05.py

--- Homework 5/sem-05.py
+++ Homework 5/sem-05.py
@@ -1,18 +1,42 @@
-class MissingParameterError(Exception):
+class MissingParameterError:
     pass
+
 class InvalidParameterError(Exception):
-    def __init__(self,invalid_parameter,*args):
-        message = f"Invalid class parameter: {invalid_parameter}"
-        super().__init__(message,*args)
+    def __init__(self, invalid_param, *args):
+        output = f"Invalid class parameter: {invalid_param}"
+        super().__init__(output, *args)
+
 class InvalidAgeError(InvalidParameterError):
-    def __init__(self,age,*args):
-        super().__init__(age,*args)
-class InvalidAgeError(InvalidParameterError):
-    def __init__(self, sound, *args):
-        super().__init__(sound, *args)
-class JungleAnimal():
-    def __init__(self,name,age,sound):
+    def __init__(self, age, *args):
+        output = f"Invalid parameter: {age}"
+        super().__init__(output, *args)
+
+class JungleAnimal:
+    def __init__(self, name, age, sound):
         self.name = name
         self.age = age
         self.sound = sound
-        
+
+        if type(name) != str:
+            raise InvalidParameterError(name)
+
+        if type(age) != int:
+            raise InvalidAgeError
+
+        if type(sound) != str:
+            raise InvalidSoundError
+
+    def make_sound(self):
+        return f"{self.name} says {self.sound}!"
+
+    def print(self):
+        pass
+
+    def daily_task(self):
+        pass
+
+class Jaguar(JungleAnimal):
+    def __init__(self,  name, age, sound):
+        super().__init__(name, age, sound)
+        if age > 15:
+            raise InvalidAgeError(age)
