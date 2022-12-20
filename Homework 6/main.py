719524aee32bf3702458f1423027de3d85e9b105
Vladislav Viktorov CSE 37 group <115734573+VladislavViktorov@users.noreply.github.com>
2022-12-20 19:49:51 +0200

    Create main.py

---
+++ Homework 6/main.py
@@ -0,0 +1,62 @@
+from bank import Bank
+from errors import InvalidUserData, InvalidMenuChoice
+
+class Menu:
+    def print_menu(self):
+        print("1. Register a new user")
+        print("2. Create an account for an existing user")
+        print("3. List all users")
+        print("4. List all accounts for an existing user")
+        print("5. Deposit money to a user account")
+        print("6. Withdraw money from a user account")
+        print("7. Exit")
+
+    def run(self):
+        bank = Bank()
+
+        # infinite menu loop
+        while True:  
+            self.print_menu()
+            choice = input("Choose an item from the menu: \n> ")
+
+            try:
+                if choice == "1":
+                    names = input("Enter the user's names (alpha-only): ")
+                    fname, lname = names.split(" ")
+                    if len(names) < 4 or not fname.isalpha() or not lname.isalpha():
+                        raise InvalidUserData("Invalid names")
+
+                    egn = input("Enter the user's EGN number (len 10, digits-only): ")
+                    if len(egn) != 10 or not egn.isdigit():
+                        raise InvalidUserData("Invalid EGN number")
+                    bank.add_user(names, egn)
+                elif choice == "2":
+                    egn = input("Enter the EGN number: ")
+                    if len(egn) != 10 or not egn.isdigit():
+                        raise InvalidUserData("Invalid EGN number")
+                    currency = input("Enter currency: ")
+                    bank.add_account(egn, currency)
+                elif choice == "3":
+                    for u in bank.users:
+                        print(u.get_print())
+                elif choice == "4":
+                    egn = input("Enter EGN number: ")
+                    if len(egn) != 10 or not egn.isdigit():
+                        raise InvalidUserData("Invalid EGN number")
+                    user = bank.find_user(egn)
+                    for k in user.accounts:
+                        print(user.accounts[k])
+                elif choice == "7":
+                    print("Goodbye\n")
+                    break
+                else:
+                    raise InvalidMenuChoice("Error: Invalid choice")
+            except Exception as ex:
+                print(f"Error: There was an error while executing the command:\n{str(ex)}")
+
+            print()
+
+
+if __name__ == '__main__':
+    menu = Menu()
+    menu.run()
